4ae9db4f2d4f345a5c7730899a7f873866c9afe0
syaiful6 <sbahri@indo-soft.com>
2021-03-16 17:50:27 +0700

    fix import error for python 2 compatible

--- djangocms_teaser/models.py
+++ djangocms_teaser/models.py
@@ -13,10 +13,8 @@ except ImportError:
         for information
         """
         return instance.get_media_path(filename)
-from cms.utils.compat.dj import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class Teaser(CMSPlugin):
     """
     A Teaser
